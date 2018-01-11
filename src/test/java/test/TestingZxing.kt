a54df2b6956993c765f0cdf1879b7e5b220a223c
Pavel Maslov <pavel.masloff@gmail.com>
2018-01-11 08:15:03 +0100

    small fix (using assertion)

--- src/test/java/test/TestingZxing.kt
+++ src/test/java/test/TestingZxing.kt
@@ -3,6 +3,7 @@ package test
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.MultiFormatReader
 import com.google.zxing.common.HybridBinarizer
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.File
 import javax.imageio.ImageIO
@@ -11,11 +12,10 @@ class TestingZxing {
 
     @Test
     fun test() {
-        println(decode("qrcode.png"))
-        println(decode("qrcode-photo.png"))
-        println(decode("barcode.jpg"))
+        assertEquals("https://www.shopify.com", decode("qrcode.png"))
+        assertEquals("MEBKM:URL:http\\://en.wikipedia.org/wiki/Main_Page;;", decode("qrcode-photo.png"))
+        assertEquals("123456", decode("barcode.jpg"))
     }
-
 
     fun decode(filename: String): String {
         val path = ClassLoader.getSystemResource(filename).file
